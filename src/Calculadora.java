3433b5d59e1751738a44107fa4ea7ce744f2cc89
rafaelranery <rafaelrapnery@gmail.com>
2023-06-25 18:08:17 -0300

    fixes em Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,6 +1,6 @@
 public class Calculadora {
 
-    public double calcularMedias(double n1, double n2, double n3, double n4) {
+    public double calcularMedia(double n1, double n2, double n3, double n4) {
         double media = ((n1 + n2 + n3 + n4) / 4);
         declararMedia(media);
         return media;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Calculadora c = new Calculadora();
 
+        double nota1 = 5.5;
+        double nota2 = 10;
+        double nota3 = 2.0;
+        double nota4 = 7.5;
+
+        double[] notas = {nota1, nota2, nota3, nota4};
+
+        c.calcularMedia(nota1, nota2, nota3, nota4);
+        c.calcularMedia(notas);
     }
 }
